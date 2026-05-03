25b01d228739860de86cf1aafcaa1cfba36b8dd6
Xiaolin Wu <shannonwxl@gmail.com>
2026-05-03 00:21:45 +0000

    Add dim_problem and dim_violation for Milestone 4

--- models/project_group15/marts/fact_cafeteria_inspections.sql
+++ models/project_group15/marts/fact_cafeteria_inspections.sql
@@ -10,12 +10,21 @@ geo AS (
     SELECT *
     FROM {{ ref('dim_geography') }}
 
+),
+
+violation AS (
+
+    SELECT *
+    FROM {{ ref('dim_violation') }}
+
 )
 
 SELECT
     GENERATE_UUID() AS inspection_id,
 
     g.geography_id,
+    v.violation_id,
+
     inspection_date,
 
     CASE
@@ -24,8 +33,6 @@ SELECT
     END AS has_violation,
 
     school_name,
-    violation_code,
-    violation_description,
     inspection_type,
     inspection_result
 
@@ -33,3 +40,8 @@ FROM source s
 LEFT JOIN geo g
     ON s.borough = g.borough
    AND s.zip_code = g.zip_code
+
+LEFT JOIN violation v
+    ON s.violation_code = v.violation_code
+   AND s.violation_description = v.violation_description
+   AND s.inspection_result = v.violation_level
